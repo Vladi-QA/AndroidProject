5dfb9b06a06c6d97a87c82b059a8368600adf7b3
Vladi-QA <volodymyr.dziuba@gmail.com>
2021-06-06 12:57:44 -0400

    First version

---
+++ app/src/main/java/com/example/doandgo_version1/SignUp3.java
@@ -0,0 +1,14 @@
+package com.example.doandgo_version1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up3);
+    }
+}
