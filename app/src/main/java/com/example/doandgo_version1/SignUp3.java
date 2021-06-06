b7da002f1237ff1d63a0216d9473f5b21a8ddfe8
Vladi-QA <volodymyr.dziuba@gmail.com>
2021-06-06 17:15:01 -0400

    second version

--- app/src/main/java/com/example/doandgo_version1/SignUp3.java
+++ app/src/main/java/com/example/doandgo_version1/SignUp3.java
@@ -2,13 +2,24 @@ package com.example.doandgo_version1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp3 extends AppCompatActivity {
-
+    Button Submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up3);
+        Submit = findViewById(R.id.Next4);
+        Submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp3.this, SignUp4.class);
+                startActivity(intent);
+            }
+        });
     }
 }
